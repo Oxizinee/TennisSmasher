b2df17cec94e79fc2574d39d9f6b19ededfb5f28
Aleksandra Zupanska <aleksandra.zupanska@student.howest.be>
2024-03-29 09:10:53 +0100

    Simple Player Movement

---
+++ TennisSlammer/Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float MovementSpeed, RotationSpeed;
+
+    private Vector2 _rotateInput, _input;
+    private Vector3 _moveVector;
+    private float _verticalVel, _gravity = 12;
+    private CharacterController _characterController;
+    private void OnRotate(InputValue value)
+    {
+        _rotateInput = value.Get<Vector2>();
+    }
+    private void OnMove(InputValue value)
+    {
+        _input = value.Get<Vector2>();
+    }
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Movement();
+    }
+    private void Rotate()
+    {
+        transform.Rotate(0, _rotateInput.x * RotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void Movement()
+    {
+        _moveVector = (transform.forward * _input.y + transform.right * _input.x) * MovementSpeed;
+        _moveVector.y = _verticalVel;
+
+        if (_characterController.isGrounded)
+        {
+            _verticalVel = -0.5f;
+        }
+        //if (_characterController.isGrounded && _isJumpingValue == 1) //Jump
+        //{
+        //    _verticalVel = JumpHeight;
+        //}
+        else
+        {
+            _verticalVel -= _gravity * Time.deltaTime;
+        }
+
+        _characterController.Move(_moveVector * Time.deltaTime);
+    }
+}
